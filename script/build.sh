3ea60747a2feae2ea33d7d2c7a5dfbbf658a4773
Alexander Sandström <mail+github@a16m.se>
2022-07-28 15:54:54 +0200

    Update build script

--- script/build.sh
+++ script/build.sh
@@ -5,8 +5,10 @@ if [ -d dist ]; then rm -rf dist; fi
 mkdir dist
 
 # UMD build
-babel src/pinch-zoom.js -o dist/pinch-zoom.umd.js
+npx babel src/pinch-zoom.js -o dist/pinch-zoom.umd.js
 
 # Minify
-babel-minify --sourceType module src/pinch-zoom.js -o dist/pinch-zoom.min.js
-babel-minify dist/pinch-zoom.umd.js -o dist/pinch-zoom.umd.min.js
+npx babel-minify --sourceType module src/pinch-zoom.js -o dist/pinch-zoom.min.js
+npx babel-minify dist/pinch-zoom.umd.js -o dist/pinch-zoom.umd.min.js
+
+echo "Build completed, see dist directory"
